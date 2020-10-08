99b324f37651aa0691ff2fdd739859d2335359c8
Pablo-Bernal <65961707+pablo-start@users.noreply.github.com>
2020-10-08 07:12:50 -0500

    Add files via upload

---
+++ Programa1.java
@@ -0,0 +1,14 @@
+
+package Githup;
+
+public class Programa1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Programa 1");
+        System.out.println("Bienvenidos");
+    }
+    
+}
